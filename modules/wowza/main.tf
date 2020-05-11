b0aea36e1e2a698e031226fafbbbefede53ecc52
Jason Paige <jason.r.paige@gmail.com>
2020-05-11 10:08:00 +0100

    fix

--- modules/wowza/main.tf
+++ modules/wowza/main.tf
@@ -207,10 +207,12 @@ resource "azurerm_lb" "lb" {
   name                = "${local.service_name}-lb"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
     public_ip_address_id = azurerm_public_ip.pip.id
+    subnet_id            = azurerm_subnet.sn.id
   }
 }
 
