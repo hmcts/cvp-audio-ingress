c88a9767a29dc9e79acbfea26c9cbf42ca6f8631
Jason Paige <jason.r.paige@gmail.com>
2020-05-11 11:35:31 +0100

    drop pipv1 from nic

--- modules/wowza/main.tf
+++ modules/wowza/main.tf
@@ -242,7 +242,6 @@ resource "azurerm_network_interface" "nic" {
     name                          = "wowzaConfiguration"
     subnet_id                     = azurerm_subnet.sn.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip_vm1.id
   }
 }
 
