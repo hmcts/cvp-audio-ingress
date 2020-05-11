f82dc58028d9e103f24a56e1510a5e8dba0c4a1a
Jason Paige <jason.r.paige@gmail.com>
2020-05-11 11:07:28 +0100

    fix sbox

--- modules/wowza/main.tf
+++ modules/wowza/main.tf
@@ -134,6 +134,31 @@ resource "azurerm_network_security_group" "sg" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 
+//  Turn on after testing complete
+//  security_rule {
+//    name                       = "REST"
+//    priority                   = 1030
+//    direction                  = "Inbound"
+//    access                     = "Allow"
+//    protocol                   = "Tcp"
+//    source_port_range          = "*"
+//    destination_port_range     = "8087"
+//    source_address_prefix      = "*"
+//    destination_address_prefix = "*"
+//  }
+//
+//  security_rule {
+//    name                       = "RTMPS"
+//    priority                   = 1040
+//    direction                  = "Inbound"
+//    access                     = "Allow"
+//    protocol                   = "Tcp"
+//    source_port_range          = "*"
+//    destination_port_range     = "443"
+//    source_address_prefix      = "*"
+//    destination_address_prefix = "*"
+//  }
+
   security_rule {
     name                       = "Server"
     priority                   = 1010
