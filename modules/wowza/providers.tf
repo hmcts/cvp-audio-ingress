5e12cd129334074fe79901cb682fc82951fe8f0b
Jason Paige <jason.r.paige@gmail.com>
2020-05-19 11:10:03 +0100

    sub id

--- modules/wowza/providers.tf
+++ modules/wowza/providers.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 
 provider "azurerm" {
   alias           = "secops"
-  subscription_id = "DCD-CNP-Prod"
+  subscription_id = "8999dec3-0104-4a27-94ee-6588559729d1"
   version         = ">= 2.7.0"
   features {}
 }
