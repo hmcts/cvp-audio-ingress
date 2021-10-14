6643c4d8ff6b06e71c7a4093deec92c2410bc277
Christopher Pateman <christopher.pateman@version1.com>
2021-10-14 15:30:55 +0100

    remove permission code

--- modules/dynatrace/main.tf
+++ modules/dynatrace/main.tf
@@ -8,29 +8,10 @@ data "azurerm_key_vault" "kv" {
   name                = local.key_vault_name
   resource_group_name = local.key_vault_resource_group_name
 }
-/* resource "azurerm_role_assignment" "kv_access" {
-  provider     = azurerm.core_infra
-  scope                = data.azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = data.azurerm_client_config.current.object_id
-}
-resource "azurerm_key_vault_access_policy" "policy" {
-  provider     = azurerm.core_infra
-  key_vault_id            = data.azurerm_key_vault.kv.id
-  tenant_id               = data.azurerm_client_config.current.tenant_id
-  object_id               = data.azurerm_client_config.current.object_id
-  key_permissions         = []
-  secret_permissions      = ["Get", "List"]
-  certificate_permissions = []
-  storage_permissions     = []
-} */
+
 data "azurerm_key_vault_secret" "dynatrace_token" {
   name         = local.dynatrace_token_name
   key_vault_id = data.azurerm_key_vault.kv.id
-  /*depends_on = [
-    azurerm_key_vault_access_policy.policy,
-    azurerm_role_assignment.kv_access
-  ] */
 }
 
 data "azurerm_client_config" "current" {}
--- modules/dynatrace/providers.tf
+++ modules/dynatrace/providers.tf
@@ -1,9 +1,3 @@
 provider "azurerm" {
   features {}
 }
-
-provider "azurerm" {
-  alias           = "core_infra"
-  subscription_id = "bf308a5c-0624-4334-8ff8-8dca9fd43783"
-  features {}
-}
