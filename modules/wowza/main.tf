778d0c6b29910efd76a484484b899d278505178b
Christopher Pateman <christopher.pateman@version1.com>
2021-11-30 14:17:01 +0000

    add public ip to LB

--- modules/wowza/main.tf
+++ modules/wowza/main.tf
@@ -230,47 +230,6 @@ resource "azurerm_network_interface" "nic2" {
   tags = var.common_tags
 }
 
-resource "azurerm_lb" "lb" {
-  name                = "${local.service_name}-lb"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "Standard"
-
-  frontend_ip_configuration {
-    name                          = "PrivateIPAddress"
-    subnet_id                     = azurerm_subnet.sn.id
-    private_ip_address            = var.lb_IPaddress
-    private_ip_address_allocation = "Static"
-  }
-  tags = var.common_tags
-}
-
-resource "azurerm_lb_backend_address_pool" "be_add_pool" {
-  loadbalancer_id = azurerm_lb.lb.id
-  name            = "BackEndAddressPool"
-}
-
-resource "azurerm_lb_probe" "lb_probe" {
-  resource_group_name = azurerm_resource_group.rg.name
-  loadbalancer_id     = azurerm_lb.lb.id
-  name                = "wowza-running-probe"
-  port                = 443
-  protocol            = "Tcp"
-}
-
-resource "azurerm_lb_rule" "rtmps_lb_rule" {
-  resource_group_name            = azurerm_resource_group.rg.name
-  loadbalancer_id                = azurerm_lb.lb.id
-  name                           = "RTMPS"
-  protocol                       = "Tcp"
-  frontend_port                  = 443
-  backend_port                   = 443
-  frontend_ip_configuration_name = azurerm_lb.lb.frontend_ip_configuration.0.name
-  backend_address_pool_id        = azurerm_lb_backend_address_pool.be_add_pool.id
-  probe_id                       = azurerm_lb_probe.lb_probe.id
-  load_distribution              = "Default"
-  idle_timeout_in_minutes        = 30
-}
 
 resource "azurerm_network_interface_security_group_association" "sg_assoc1" {
   network_interface_id      = azurerm_network_interface.nic1.id
