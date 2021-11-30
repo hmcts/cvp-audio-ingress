7b3e890f63bef76e66b6d6ce102cd7f8f39e5cc6
Christopher Pateman <christopher.pateman@version1.com>
2021-11-30 14:27:51 +0000

    remove cert install

--- modules/wowza/main.tf
+++ modules/wowza/main.tf
@@ -270,12 +270,6 @@ resource "azurerm_network_interface_backend_address_pool_association" "be_add_po
   backend_address_pool_id = azurerm_lb_backend_address_pool.be_add_pool.id
 }
 
-resource "random_password" "certPassword" {
-  length           = 32
-  special          = true
-  override_special = "_%*"
-}
-
 resource "random_password" "restPassword" {
   length           = 32
   special          = true
@@ -291,8 +285,6 @@ resource "random_password" "streamPassword" {
 data "template_file" "cloudconfig" {
   template = file(var.cloud_init_file)
   vars = {
-    certPassword       = random_password.certPassword.result
-    certThumbprint     = module.cert.thumbprint
     storageAccountName = module.sa.storageaccount_name
     storageAccountKey  = module.sa.storageaccount_primary_access_key
     restPassword       = md5("wowza:Wowza:${random_password.restPassword.result}")
@@ -318,13 +310,6 @@ data "azurerm_key_vault" "cvp_kv" {
   resource_group_name = "cvp-sharedinfra-${var.env}"
 }
 data "azurerm_client_config" "current" {
-}
-module "cert" {
-  source            = "git::https://github.com/hmcts/terraform-module-certificate.git?ref=master"
-  environment       = var.env
-  domain_dns_prefix = local.domain_dns_prefix
-  domain_prefix     = "cvp-recording"
-  object_id         = data.azurerm_client_config.current.object_id
 }
 data "azurerm_key_vault_secret" "ssh_pub_key" {
   name         = "cvp-ssh-pub-key"
@@ -360,12 +345,6 @@ resource "azurerm_linux_virtual_machine" "vm1" {
   }
 
   provision_vm_agent = true
-  secret {
-    certificate {
-      url = module.cert.secret_id
-    }
-    key_vault_id = module.cert.key_vault_id
-  }
 
   custom_data = data.template_cloudinit_config.wowza_setup.rendered
 
@@ -417,12 +396,6 @@ resource "azurerm_linux_virtual_machine" "vm2" {
   }
 
   provision_vm_agent = true
-  secret {
-    certificate {
-      url = module.cert.secret_id
-    }
-    key_vault_id = module.cert.key_vault_id
-  }
 
   custom_data = data.template_cloudinit_config.wowza_setup.rendered
 
