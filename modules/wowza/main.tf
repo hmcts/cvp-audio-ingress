61dfebfb36d46af7fdeba0b4f5e4530f5e7b74b8
Adam Todd <adam.todd@hmcts.net>
2022-02-02 16:37:44 +0000

    Registration enabled to true

--- modules/wowza/main.tf
+++ modules/wowza/main.tf
@@ -55,12 +55,12 @@ data "azurerm_private_dns_zone" "blob" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link" {
   provider = azurerm.shared-dns-zone
-  
+
   name                  = "${azurerm_virtual_network.vnet.name}-link"
   resource_group_name   = "core-infra-intsvc-rg"
   private_dns_zone_name = data.azurerm_private_dns_zone.blob.name
   virtual_network_id    = azurerm_virtual_network.vnet.id
-  registration_enabled  = false
+  registration_enabled  = true
   tags                  = var.common_tags
 }
 
