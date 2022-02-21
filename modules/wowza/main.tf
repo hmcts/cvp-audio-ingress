27d7c9bed91f8d6ab25d0b9879442653199e21a0
Adam Todd <adam.todd@hmcts.net>
2022-02-21 15:56:19 +0000

    comment out vnet link

--- modules/wowza/main.tf
+++ modules/wowza/main.tf
@@ -52,17 +52,17 @@ data "azurerm_private_dns_zone" "blob" {
   resource_group_name = "core-infra-intsvc-rg"
 }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link" {
-  provider = azurerm.shared-dns-zone
-  
-  name                  = "${azurerm_virtual_network.vnet.name}-link"
-  resource_group_name   = "core-infra-intsvc-rg"
-  private_dns_zone_name = data.azurerm_private_dns_zone.blob.name
-  virtual_network_id    = azurerm_virtual_network.vnet.id
-  registration_enabled  = false
-  tags                  = var.common_tags
-}
-
+#resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link" {
+#  provider = azurerm.shared-dns-zone
+#  
+#  name                  = "${azurerm_virtual_network.vnet.name}-link"
+#  resource_group_name   = azurerm_resource_group.rg.name
+#  private_dns_zone_name = data.azurerm_private_dns_zone.blob.name
+#  virtual_network_id    = azurerm_virtual_network.vnet.id
+#  registration_enabled  = false
+#  tags                  = var.common_tags
+#}
+#
 resource "azurerm_private_dns_a_record" "sa_a_record" {
   provider = azurerm.shared-dns-zone
   
