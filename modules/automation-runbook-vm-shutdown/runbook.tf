b5748cd41a24f14a6c867dcb1a04e7b6abc4f929
rmurphy-hmcts <robert.murphy@HMCTS.NET>
2022-02-21 10:58:46 +0000

    aa account

--- modules/automation-runbook-vm-shutdown/runbook.tf
+++ modules/automation-runbook-vm-shutdown/runbook.tf
@@ -1,11 +1,11 @@
 
-locals {
-  runbook_name    = "vm-shutdown.ps1"
-  runbook_content = file("${path.module}/${local.runbook_name}")
-}
+# locals {
+#   runbook_name    = "vm-shutdown.ps1"
+#   runbook_content = file("./${local.runbook_name}")
+# }
 
 resource "azurerm_automation_account" "automation_account" {
-  name                = var.aa_name
+  name                = var.automation_account_name
   location            = var.location
   resource_group_name = var.resource_group_name
   sku_name            = var.automation_account_sku_name
