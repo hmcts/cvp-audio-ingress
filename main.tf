a00fd4097ad26befebfcccb12659b610a60417d7
rmurphy-hmcts <robert.murphy@HMCTS.NET>
2022-03-01 12:48:52 +0000

    auto account off

--- main.tf
+++ main.tf
@@ -48,39 +48,39 @@ resource "azurerm_dns_a_record" "wowza" {
 # =================    automation account    ======================
 # =================================================================
 
-resource "azurerm_automation_account" "vm-start-stop" {
-  name                = "${var.product}-recordings-${var.env}-aa2"
-  location            = var.location
-  resource_group_name = "${var.product}-recordings-${var.env}-rg"
-  sku_name            = var.automation_account_sku_name
+# resource "azurerm_automation_account" "vm-start-stop" {
+#   name                = "${var.product}-recordings-${var.env}-aa"
+#   location            = var.location
+#   resource_group_name = "${var.product}-recordings-${var.env}-rg"
+#   sku_name            = var.automation_account_sku_name
 
-  identity {
-    type         = "UserAssigned"
-    identity_ids = [module.vm_automation.cvp_aa_mi_id]
-  }
+#   identity {
+#     type         = "UserAssigned"
+#     identity_ids = [module.vm_automation.cvp_aa_mi_id]
+#   }
 
-  tags = local.common_tags
-}
+#   tags = local.common_tags
+# }
 
-#  vm shutdown/start runbook
-module "vm_automation" {
-  source                  = "./modules/automation-runbook-vm-shutdown"
-  automation_account_name = azurerm_automation_account.vm-start-stop.name
-  location                = var.location
-  env                     = var.env
-  resource_group_id       = module.wowza.wowza_rg_id
-  vm_status               = var.vm_status
-  runbook_schedule_times  = var.runbook_schedule_times
-  tags                    = local.common_tags
-  auto_acc_runbook_names = {
-    resource_group_name         = "${var.product}-recordings-${var.env}-rg"
-    runbook_name                = "${var.product}-recordings-VM-start-stop-${var.env}2"
-    schedule_name               = "${var.product}-recordings-schedule-${var.env}2"
-    job_schedule_name           = "${var.product}-recordings-schedule-${var.env}2"
-    user_assigned_identity_name = "${var.product}-recordings-automation-mi-${var.env}2"
-    role_definition_name        = "${var.product}-recordings-vm-control-${var.env}2"
-    script_name                 = var.script_name
-    vm_names                    = join(",", [module.wowza.vm1_name, module.wowza.vm2_name])
-  }
-}
+# #  vm shutdown/start runbook
+# module "vm_automation" {
+#   source                  = "./modules/automation-runbook-vm-shutdown"
+#   automation_account_name = azurerm_automation_account.vm-start-stop.name
+#   location                = var.location
+#   env                     = var.env
+#   resource_group_id       = module.wowza.wowza_rg_id
+#   vm_status               = var.vm_status
+#   runbook_schedule_times  = var.runbook_schedule_times
+#   tags                    = local.common_tags
+#   auto_acc_runbook_names = {
+#     resource_group_name         = "${var.product}-recordings-${var.env}-rg"
+#     runbook_name                = "${var.product}-recordings-VM-start-stop-${var.env}"
+#     schedule_name               = "${var.product}-recordings-schedule-${var.env}"
+#     job_schedule_name           = "${var.product}-recordings-schedule-${var.env}"
+#     user_assigned_identity_name = "${var.product}-recordings-automation-mi-${var.env}"
+#     role_definition_name        = "${var.product}-recordings-vm-control-${var.env}"
+#     script_name                 = var.script_name
+#     vm_names                    = join(",", [module.wowza.vm1_name, module.wowza.vm2_name])
+#   }
+# }
 
