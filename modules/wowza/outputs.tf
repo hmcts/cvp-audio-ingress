27193ff30e63c451b422a3ab34c06dc83571d364
rmurphy-hmcts <robert.murphy@HMCTS.NET>
2022-03-11 15:56:06 +0000

    unused outputs removed

--- modules/wowza/outputs.tf
+++ modules/wowza/outputs.tf
@@ -2,15 +2,6 @@ output "vnet_id" {
   description = ""
   value       = azurerm_virtual_network.vnet.id
 }
-
-output "vm1_name" {
-  description = "name of virtual machine 1"
-  value       = azurerm_linux_virtual_machine.vm1.name
-}
-output "vm2_name" {
-  description = "name of virtual machine 2"
-  value       = azurerm_linux_virtual_machine.vm2.name
-}
 output "rest_password" {
   description = ""
   value       = random_password.restPassword.result
@@ -26,7 +17,3 @@ output "stream_password" {
 //   description = "Public IP for LB - This needs to be added to https://github.com/hmcts/azure-public-dns"
 //   value       = azurerm_private_ip.pip.ip_address
 // }
-# output "aa_mi_id" {
-#   description = "automation account managed identity id"
-#   value       = module.vm_automation.aa_mi_id
-# }
