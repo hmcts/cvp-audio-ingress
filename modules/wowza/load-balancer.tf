b0463c2dad4eb2d27fb92f99a7a02637987349d5
Your Name <james.ferrari@version1.com>
2022-09-16 13:03:12 +0100

    amend backend pool for lb rule

--- modules/wowza/load-balancer.tf
+++ modules/wowza/load-balancer.tf
@@ -34,7 +34,7 @@ resource "azurerm_lb_rule" "rtmps_lb_rule" {
   frontend_port                  = 443
   backend_port                   = 443
   frontend_ip_configuration_name = azurerm_lb.lb.frontend_ip_configuration.0.name
-  backend_address_pool_id        = azurerm_lb_backend_address_pool.be_add_pool.id
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.be_add_pool.id]
   probe_id                       = azurerm_lb_probe.lb_probe.id
   load_distribution              = "Default"
   idle_timeout_in_minutes        = 30
