c476a085bc55a3d7dc675f0203ec306074a99757
Thomas Watson <thomas.watson@version1.com>
2023-01-24 17:34:21 +0000

    added test alert

--- modules/wowza/backup.tf
+++ modules/wowza/backup.tf
@@ -44,3 +44,36 @@ resource "azurerm_backup_protected_vm" "vms-backup" {
   source_vm_id        = each.value
   backup_policy_id    = azurerm_backup_policy_vm.vm_backup.id
 }
+
+resource "azurerm_monitor_action_group" "cvp-ag" {
+  name                = "${local.service_name}-backup-alerts-ag"
+  resource_group_name = azurerm_resource_group.rg.name
+  short_name          = "CVPBackup"
+
+  email_receiver {
+    name                    = "Tom Test"
+    email_address           = "thomas.watson@hmcts.net"
+    use_common_alert_schema = true
+  }
+}
+
+resource "azurerm_monitor_metric_alert" "cvp-backup-alert" {
+  name                = "${local.service_name}-backup-alert"
+  resource_group_name = azurerm_resource_group.rg.name
+  scopes              = azurerm_recovery_services_vault.backup_vault.id
+  description         = "Alert will be triggered when a Backup event happens."
+  tags                = var.common_tags
+
+  criteria {
+    metric_namespace = "Microsoft.RecoveryServices/vaults"
+    metric_name      = "BackupHealthEvent"
+    aggregation      = "Count"
+    operator         = "GreaterThan"
+    threshold        = 0
+
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.cvp-ag.id
+  }
+}
