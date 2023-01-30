e1cd21c364b59210937afdf117acd02c2542fa87
Benjamin Garside (v1) <benjamin.garside@version1.com>
2023-01-30 16:18:35 +0000

    upping TF version to 1.3.7

--- terraform/_versions.tf
+++ terraform/_versions.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
   }
-  required_version = ">= 1.0.4"
+  required_version = ">= 1.3.7"
   required_providers {
     azurerm  = ">= 2.34.0"
     template = "~> 2.1"
