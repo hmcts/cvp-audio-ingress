c6a31c3adf44702785f8535e566b16bc95d74c0c
Benjamin Garside (v1) <benjamin.garside@version1.com>
2023-02-08 11:45:29 +0000

    correct kv name

--- terraform/automation-account-sas-token-renewal.tf
+++ terraform/automation-account-sas-token-renewal.tf
@@ -13,7 +13,7 @@ module "automation_runbook_sas_token_renewal" {
   storage_account_name    = each.value.storage_account
   container_name          = each.value.container
   blob_name               = each.value.blob
-  key_vault_name          = local.key_vault_name
+  key_vault_name          = data.azurerm_key_vault.cvp_kv.name
   secret_name             = "cvp-sas-${each.value.container}-${each.value.blob}-${each.value.permissions}"
   expiry_date             = each.value.expiry_date
   automation_account_name = azurerm_automation_account.cvp.name
