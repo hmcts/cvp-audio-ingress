e1f08e0455fc170e75ad606a27a41f5882972a78
Benjamin Garside (v1) <benjamin.garside@version1.com>
2023-02-10 17:36:09 +0000

    fix output name

--- terraform/monitor-action-group.tf
+++ terraform/monitor-action-group.tf
@@ -12,7 +12,7 @@ resource "azurerm_monitor_action_group" "cvp-backup-ag" {
   automation_runbook_receiver {
     name                    = "CVP - '${local.service_name}-vm${count.index + 1}' backup not healthy."
     automation_account_id   = azurerm_automation_account.cvp.id
-    runbook_name            = module.dynatrace_runbook.name
+    runbook_name            = module.dynatrace_runbook.runbook_name
     webhook_resource_id     = azurerm_automation_webhook.webhook_back_unhealthy[0].id
     is_global_runbook       = true
     service_uri             = azurerm_automation_webhook.webhook_back_unhealthy[0].uri
