90534f0bcc12469582cf55f5f898db1077dd66fe
Benjamin Garside (v1) <benjamin.garside@version1.com>
2023-02-13 14:07:50 +0000

    remove unneeded variable

--- terraform/_variables.tf
+++ terraform/_variables.tf
@@ -119,14 +119,6 @@ variable "num_applications" {
   description = "Number of Wowza applications to create as part of cloudinit, defaults to 1."
 }
 
-variable "vm_backup_alert_emails" {
-  type = list(object({
-    name  = string
-    email = string
-  }))
-  description = "(Required) Email adresses to recieve VM backup alerts."
-}
-
 #---------------------------------------------------
 # Networking settings
 #---------------------------------------------------
