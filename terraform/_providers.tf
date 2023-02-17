dbf6cb7bccc42c9c0cbe0e76c479405da50ac7ef
Benjamin Garside (v1) <benjamin.garside@version1.com>
2023-02-17 15:49:17 +0000

    adding VPN peering

--- terraform/_providers.tf
+++ terraform/_providers.tf
@@ -13,3 +13,21 @@ provider "azurerm" {
   subscription_id = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
   features {}
 }
+
+provider "azurerm" {
+  features {}
+  alias           = "peering_target_vpn"
+  subscription_id = local.peering_vpn_subscription
+  client_id       = var.network_client_id
+  client_secret   = var.network_client_secret
+  tenant_id       = var.network_tenant_id
+}
+
+provider "azurerm" {
+  features {}
+  alias           = "peering_client"
+  subscription_id = data.azurerm_client_config.current.subscription_id
+  client_id       = var.network_client_id
+  client_secret   = var.network_client_secret
+  tenant_id       = var.network_tenant_id
+}
