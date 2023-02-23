b77b1d7cf6f12489640e9314e7d6bd9e6349b848
Benjamin Garside (v1) <benjamin.garside@version1.com>
2023-02-23 15:17:51 +0000

    reformat

--- terraform/_data.tf
+++ terraform/_data.tf
@@ -2,12 +2,12 @@
 # Data resources
 #---------------------------------------------------
 
+data "azurerm_client_config" "current" {
+}
+
 data "azurerm_key_vault" "cvp_kv" {
   name                = "cvp-${var.env}-kv"
   resource_group_name = "cvp-sharedinfra-${var.env}"
-}
-
-data "azurerm_client_config" "current" {
 }
 
 data "azurerm_key_vault_secret" "ssh_pub_key" {
