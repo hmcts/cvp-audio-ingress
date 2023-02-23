156d021cf2700f2e07e5b2284de90829a491506f
Benjamin Garside (v1) <benjamin.garside@version1.com>
2023-02-23 15:00:07 +0000

    typo

--- terraform/key-vault-access-policies.tf
+++ terraform/key-vault-access-policies.tf
@@ -3,9 +3,9 @@
 #---------------------------------------------------
 
 resource "azurerm_key_vault_access_policy" "sas_renewal" {
-  key_vault_id            = azurerm_key_vault.cvp_kv.id
-  tenant_id               = azurerm_client_config.current.tenant_id
-  object_id               = data.azurerm_key_vault.cvp_kv.id
+  key_vault_id            = data.azurerm_key_vault.cvp_kv.id
+  tenant_id               = data.azurerm_client_config.current.tenant_id
+  object_id               = azurerm_automation_account.cvp.id
 
   key_permissions         = []
   secret_permissions      = ["Get", "List", "Set", "Delete"]
