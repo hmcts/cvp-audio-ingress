62a5805008c1018922b3ea6656c5b9db82d5e731
Benjamin Garside (v1) <benjamin.garside@version1.com>
2023-02-24 13:50:43 +0000

    add new vars

--- terraform/automation-account-sas-token-renewal.tf
+++ terraform/automation-account-sas-token-renewal.tf
@@ -3,20 +3,23 @@
 #---------------------------------------------------
 module "automation_runbook_sas_token_renewal" {
   source = "git::https://github.com/hmcts/cnp-module-automation-runbook-sas-token-renewal?ref=add-umi-and-network-bypass"
-  
+
   for_each = local.sas_tokens
 
   name = "rotate-sas-tokens-${each.value.storage_account}-${each.value.container}-${each.value.blob}-${each.value.permissions}"
 
-  resource_group_name     = azurerm_resource_group.rg.name
-  environment             = var.env
-  storage_account_name    = each.value.storage_account
-  container_name          = each.value.container
-  blob_name               = each.value.blob
-  key_vault_name          = data.azurerm_key_vault.cvp_kv.name
-  secret_name             = "cvp-sas-${each.value.container}-${each.value.blob}-${each.value.permissions}"
-  expiry_date             = each.value.expiry_date
-  automation_account_name = azurerm_automation_account.cvp.name
+  resource_group_name              = azurerm_resource_group.rg.name
+  environment                      = var.env
+  storage_account_name             = each.value.storage_account
+  container_name                   = each.value.container
+  blob_name                        = each.value.blob
+  key_vault_name                   = data.azurerm_key_vault.cvp_kv.name
+  secret_name                      = "cvp-sas-${each.value.container}-${each.value.blob}-${each.value.permissions}"
+  expiry_date                      = each.value.expiry_date
+  automation_account_name          = azurerm_automation_account.cvp.name
+  sas_permissions                  = each.value.permissions
+  bypass_kv_networking             = true
+  user_assigned_identity_client_id = azurerm_user_assigned_identity.mi.principal_id
 
   tags = module.ctags.common_tags
 
