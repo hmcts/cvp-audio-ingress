d31dc177ee0a30e0c5c8c544cf76d24dca24e1f5
Benjamin Garside (v1) <benjamin.garside@version1.com>
2023-02-24 15:50:46 +0000

    use new sas module

--- terraform/automation-account-sas-token-renewal.tf
+++ terraform/automation-account-sas-token-renewal.tf
@@ -2,7 +2,7 @@
 # SAS token renewal runbook (via module)
 #---------------------------------------------------
 module "automation_runbook_sas_token_renewal" {
-  source = "git::https://github.com/hmcts/cnp-module-automation-runbook-sas-token-renewal?ref=add-umi-and-network-bypass"
+  source = "git::https://github.com/hmcts/cnp-module-automation-runbook-sas-token-renewal?ref=master"
 
   for_each = local.sas_tokens
 
