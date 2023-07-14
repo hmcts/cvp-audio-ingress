06d8a6225800b8416498a9ffa61b0112dab1739c
Benjamin Garside (v1) <benjamin.garside@version1.com>
2023-07-14 17:13:53 +0100

    use ext local rather than module

--- terraform/virtual-machine-splunk.tf
+++ terraform/virtual-machine-splunk.tf
@@ -1,41 +1,27 @@
 #---------------------------------------------------
-# Splunk Ext (via module)
+# Splunk Ext 
 #---------------------------------------------------
-module "splunk-uf" {
-  source = "git::https://github.com/hmcts/terraform-module-splunk-universal-forwarder.git?ref=master"
 
-  count = var.vm_count
-
-  auto_upgrade_minor_version = true
-
-  virtual_machine_type = "vm"
-  virtual_machine_id   = azurerm_linux_virtual_machine.wowza_vm[count.index].id
-
-  splunk_username = local.splunk_admin_username
-  splunk_password = random_password.splunk_admin_password.result
-
-  tags = module.ctags.common_tags
-
-  depends_on = [ 
-    azurerm_virtual_machine_extension.acl
-  ]
-
+locals {
+  cse_script = "sudo apt-get install -y acl && ./install-splunk-forwarder-service.sh ${local.splunk_admin_username} ${random_password.splunk_admin_password.result} dynatrace_forwarders"
+  script_uri = "https://raw.githubusercontent.com/hmcts/terraform-module-splunk-universal-forwarder/master/scripts/install-splunk-forwarder-service.sh"
 }
 
 resource "azurerm_virtual_machine_extension" "acl" {
 
   count = var.vm_count
 
-  name                       = "install-acl"
+  name                       = "splunk-universal-forwarder"
   virtual_machine_id         = azurerm_linux_virtual_machine.wowza_vm[count.index].id
   publisher                  = "Microsoft.Azure.Extensions"
   type                       = "CustomScript"
-  type_handler_version       = "2.0"
-  auto_upgrade_minor_version = false
+  type_handler_version       = "2.1"
+  auto_upgrade_minor_version = true
   tags                       = module.ctags.common_tags
   protected_settings         = <<PROTECTED_SETTINGS
     {
-      "commandToExecute": "sudo apt-get install -y acl"
+      "fileUris": ["${local.script_uri}"],
+      "commandToExecute": "${local.cse_script}"
     }
     PROTECTED_SETTINGS
 }
