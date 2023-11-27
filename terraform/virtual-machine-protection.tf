8524124db63ca92817c4e19fd99eebfd219e9bcc
Benjamin Garside (v1) <benjamin.garside@version1.com>
2023-11-27 16:44:56 +0000

    reducr Disk size of non-prod

--- terraform/virtual-machine-protection.tf
+++ terraform/virtual-machine-protection.tf
@@ -2,7 +2,7 @@
 # Add protection to each VM
 #---------------------------------------------------
 resource "azurerm_backup_protected_vm" "vms-backup" {
-  count = var.vm_count
+  count = var.env == "prod" ? var.vm_count : 0
 
   resource_group_name = azurerm_resource_group.rg.name
   recovery_vault_name = azurerm_recovery_services_vault.backup_vault.name
