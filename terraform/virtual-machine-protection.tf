72c262bfd477060ba249a719fd4b79e909039f24
Benjamin Garside (v1) <benjamin.garside@version1.com>
2023-11-28 12:28:23 +0000

    update ref

--- terraform/virtual-machine-protection.tf
+++ terraform/virtual-machine-protection.tf
@@ -6,7 +6,7 @@ resource "azurerm_backup_protected_vm" "vms-backup" {
 
   resource_group_name = azurerm_resource_group.rg.name
   recovery_vault_name = azurerm_recovery_services_vault.backup_vault[0].name
-  backup_policy_id    = azurerm_backup_policy_vm.vm_backup.id
+  backup_policy_id    = azurerm_backup_policy_vm.vm_backup[0].id
   source_vm_id        = azurerm_linux_virtual_machine.wowza_vm[count.index].id
 
 }
