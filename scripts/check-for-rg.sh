213cae5d9edacb24fa7acb1c95b9d29953963e2b
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-04-08 14:54:17 +0100

    echo from script

--- scripts/check-for-rg.sh
+++ scripts/check-for-rg.sh
@@ -6,7 +6,9 @@ groups=$(az group list --subscription $subscription --query  "[?name=='cvp-recor
 if [[ "${groups[@]}" == "[]" ]]; then
     #if the resource group doesn't exist, output false
     echo "##vso[task.setvariable variable=resource_group_exists;isOutput=true]false"
+    echo "Resource group was not found"
 else
     #if the resource group does exist, output true
     echo "##vso[task.setvariable variable=resource_group_exists;isOutput=true]true"
+    echo "Resource group exists"
 fi
